1bff0c8b48b0d3428a96917ab3e497ed5c5a0731
Andres Frei <andresfrei@gmail.com>
2024-09-15 01:18:11 -0300

    apollo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -23,9 +25,13 @@ import { EmailModule } from './email/email.module';
       autoLoadEntities: true,
       synchronize: true, // Cambiar a false en producción
     }),
-    GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       context: ({ req }) => ({ user: req.user }),
+      //debug:false,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     UsersModule,
     AuthModule,
